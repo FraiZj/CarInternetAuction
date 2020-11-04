8ba4dc7863657723100badd77d30fbbd7c2765d3
fra1z <37879391+FraiZj@users.noreply.github.com>
2020-11-04 13:34:21 +0200

    added BetRepository

---
+++ InternetAuction.DAL/Interfaces/IBetRepository.cs
@@ -0,0 +1,13 @@
+﻿using InternetAuction.DAL.Entities;
+using InternetAuction.DAL.Interfaces.Base;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetAuction.DAL.Interfaces
+{
+    public interface IBetRepository : IRepository<Bet>
+    {
+        IQueryable<Bet> FindAllWithDetails();
+        Task<Bet> GetByIdWithDetailsAsync(int id);
+    }
+}
---
+++ InternetAuction.DAL/Repositories/BetRepository.cs
@@ -0,0 +1,32 @@
+﻿using InternetAuction.DAL.Entities;
+using InternetAuction.DAL.Interfaces;
+using InternetAuction.DAL.Repositories.Base;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetAuction.DAL.Repositories
+{
+    public class BetRepository : Repository<Bet>, IBetRepository
+    {
+        public BetRepository(ApplicationDbContext context)
+            : base(context)
+        { }
+
+        public IQueryable<Bet> FindAllWithDetails()
+        {
+            return _entities
+                .Include(e => e.User)
+                .Include(e => e.Lot)
+                .AsQueryable();
+        }
+
+        public async Task<Bet> GetByIdWithDetailsAsync(int id)
+        {
+            return await _entities
+                .Include(e => e.User)
+                .Include(e => e.Lot)
+                .FirstOrDefaultAsync(e => e.Id == id);
+        }
+    }
+}
