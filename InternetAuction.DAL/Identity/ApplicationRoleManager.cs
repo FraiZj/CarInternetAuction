ec9ddabdaf371acf130c9edca108835fa89368a4
fra1z <37879391+FraiZj@users.noreply.github.com>
2020-11-05 13:58:22 +0200

    added identities managers

---
+++ InternetAuction.DAL/Identity/ApplicationRoleManager.cs
@@ -0,0 +1,17 @@
+﻿using InternetAuction.DAL.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace InternetAuction.DAL.Identity
+{
+    public class ApplicationRoleManager : RoleManager<ApplicationRole>
+    {
+        public ApplicationRoleManager()
+            : base(new RoleStore<ApplicationRole>())
+        { }
+
+        public ApplicationRoleManager(RoleStore<ApplicationRole> store)
+            : base(store)
+        { }
+    }
+}
---
+++ InternetAuction.DAL/Identity/ApplicationUserManager.cs
@@ -0,0 +1,17 @@
+﻿using InternetAuction.DAL.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace InternetAuction.DAL.Identity
+{
+    public class ApplicationUserManager : UserManager<ApplicationUser>
+    {
+        public ApplicationUserManager()
+            : base(new UserStore<ApplicationUser>())
+        { }
+
+        public ApplicationUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        { }
+    }
+}
