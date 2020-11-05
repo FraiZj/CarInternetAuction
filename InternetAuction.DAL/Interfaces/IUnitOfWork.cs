7b5de53485a19ef23461652580c16a304d74aed9
fra1z <37879391+FraiZj@users.noreply.github.com>
2020-11-05 15:04:45 +0200

    added unit of work

---
+++ InternetAuction.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using InternetAuction.DAL.Identity;
+using System;
+using System.Threading.Tasks;
+
+namespace InternetAuction.DAL.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ApplicationUserManager ApplicationUserManager { get; }
+        ApplicationRoleManager ApplicationRoleManager { get; }
+        ICarRepository CarRepository { get; }
+        ILotRepository LotRepository { get; }
+        IBetRepository BetRepository { get; }
+        Task SaveAsync();
+    }
+}
---
+++ InternetAuction.DAL/UnitOfWork.cs
@@ -0,0 +1,59 @@
+﻿using InternetAuction.DAL.Entities;
+using InternetAuction.DAL.Identity;
+using InternetAuction.DAL.Interfaces;
+using InternetAuction.DAL.Repositories;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Threading.Tasks;
+
+namespace InternetAuction.DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        private bool disposed;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            ApplicationUserManager = new ApplicationUserManager(new UserStore<ApplicationUser>());
+            ApplicationRoleManager = new ApplicationRoleManager(new RoleStore<ApplicationRole>());
+            CarRepository = new CarRepository(context);
+            LotRepository = new LotRepository(context);
+            BetRepository = new BetRepository(context);
+        }
+
+        public ApplicationUserManager ApplicationUserManager { get;  }
+
+        public ApplicationRoleManager ApplicationRoleManager { get; }
+
+        public ICarRepository CarRepository { get; }
+
+        public ILotRepository LotRepository { get; }
+
+        public IBetRepository BetRepository { get; }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
