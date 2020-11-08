62438a4d4aa1f9a186f3d151c3ead4119b0b9fca
fra1z <37879391+FraiZj@users.noreply.github.com>
2020-11-08 15:59:59 +0200

    Add InternetAuction.Web project

---
+++ InternetAuction.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InternetAuction.Web.Startup))]
+namespace InternetAuction.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
