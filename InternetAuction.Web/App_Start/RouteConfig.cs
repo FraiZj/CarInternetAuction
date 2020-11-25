f387cc60f6dafc9f72f495a365cff659cc5cc069
fra1z <37879391+FraiZj@users.noreply.github.com>
2020-11-26 01:35:30 +0200

    Update routes

--- InternetAuction.Web/App_Start/RouteConfig.cs
+++ InternetAuction.Web/App_Start/RouteConfig.cs
@@ -10,6 +10,12 @@ namespace InternetAuction.Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "AccountDefaultRoute",
+                url: "Account/{action}/{id}",
+                defaults: new { controller = "Account", action = "UserProfile", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Lots", action = "ActiveLots", id = UrlParameter.Optional }
